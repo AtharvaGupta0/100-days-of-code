0c4a7c23055c305959a47556851189276a15afad
Atharva Gupta <guptaatharva2007@gmail.com>
2026-08-15 16:43:39 +0000

    Update Day5Q10.c

--- Day5Q10.c
+++ Day5Q10.c
@@ -12,3 +12,20 @@ Output 2:
 2:2:2
 
 */
+
+#include <stdio.h>
+
+int main() {
+    int seconds, hours, minutes;
+
+    scanf("%d", &seconds);
+
+    hours = seconds / 3600;
+    seconds = seconds % 3600;
+    minutes = seconds / 60;
+    seconds = seconds % 60;
+
+    printf("%d:%d:%d", hours, minutes, seconds);
+
+    return 0;
+}
