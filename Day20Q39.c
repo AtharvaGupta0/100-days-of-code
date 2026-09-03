d49a9a9da7f49188eabcc92034ae782b8dba9be5
Atharva Gupta <guptaatharva2007@gmail.com>
2026-09-03 07:37:05 +0000

    Remove unnecessary checks for odd digits in product

--- Day20Q39.c
+++ Day20Q39.c
@@ -16,25 +16,17 @@ Output 2:
 #include <stdio.h>
 
 int main() {
-    int n, product = 1, hasOdd = 0;
+    int n, product = 1;
     printf("Enter a number: ");
     scanf("%d", &n);
-
-    if (n == 0) {
-        printf("1\n");
-        return 0;
-    }
 
     while (n != 0) {
         int digit = n % 10;
         if (digit % 2 != 0) {
             product *= digit;
-            hasOdd = 1;
         }
         n /= 10;
     }
-
-    if (!hasOdd) product = 1;
     printf("%d\n", product);
     return 0;
 }
