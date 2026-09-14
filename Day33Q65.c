6692c4a4f33c7e9de44141c2975bda0b3e0be674
Atharva Gupta <guptaatharva2007@gmail.com>
2026-09-14 13:04:38 +0530

    modified codes

--- Day33Q65.c
+++ Day33Q65.c
@@ -18,22 +18,28 @@ Output 2:
 */
 #include <stdio.h>
 int main() {
-  int n;
-  printf("Enter the number of elements in the array: ");
-  scanf("%d", &n);
-
-  int arr[n];
-  for (int i = 0; i < n; i++) {
+  int s, size;
+  printf("Enter the size of array: ");
+  scanf("%d", &size);
+  int arr[size];
+  for (int i = 0; i < size; i++) {
     printf("Enter %d element: ", i + 1);
     scanf("%d", &arr[i]);
   }
-  printf("Enter the element to search: ");
-  int search;
-  scanf("%d", &search);
-  for (int i = 0; i < n; i++) {
-    if (arr[i] == search) {
-      printf("Found at index %d", i);
+  printf("\nEnter the element to search: ");
+  scanf("%d", &s);
+  int low = 0;
+  int high = size - 1;
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    if (arr[mid] == s) {
+      printf("Found at index %d", mid);
       return 0;
+    }
+    if (arr[mid] < s) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
   printf("-1");
